050a5960bd3a308f67b8366172efc6baa411351b
Dafter <dafterplay@mail.ru>
2023-02-09 20:03:57 +0300

    add drawing graphs

--- main.py
+++ main.py
@@ -9,16 +9,35 @@
 import numpy as np
 from scipy.integrate import quad
 from scipy.interpolate import lagrange, CubicSpline
+import matplotlib.pyplot as plt
 from prettytable import PrettyTable
 
 
-def print_interpolation(x_find, y_real, y_lagrange, y_spline):
+def print_interpolation_table(x_find, y_real, y_lagrange, y_spline):
     pt = PrettyTable()
     pt.add_column('x', np.round(x_find, 4))
     pt.add_column('real y', y_real)
     pt.add_column('lagrange y', y_lagrange)
     pt.add_column('spline y', y_spline)
     print(pt)
+
+
+def print_one_graph(x, y, title, id):
+    plt.subplot(1, 3, id)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.title(title)
+    plt.plot(x, y)
+
+
+def print_interpolation_graph(x_find, y_real, y_lagrange, y_spline):
+    plt.figure(figsize=(15, 4))
+    print_one_graph(x_find, y_real, 'Исходный график', 1)
+    print_one_graph(x_find, y_lagrange, 'График Лагранжем', 2)
+    print_one_graph(x_find, y_spline, 'График сплайном', 3)
+    # plt.savefig("image.jpg")
+    plt.show()
 
 
 def interpolation():
@@ -34,7 +53,8 @@ def interpolation():
     # Строим сплайн интерполяцию
     spline_interpolation = CubicSpline(x_interpolation, y_interpolation)
     y_spline = spline_interpolation(x_find)
-    print_interpolation(x_find, y_real, y_lagrange, y_spline)
+    print_interpolation_table(x_find, y_real, y_lagrange, y_spline)
+    print_interpolation_graph(x_find, y_real, y_lagrange, y_spline)
 
 
 def pseudo_quanc8(func, start, end):
